57214048e627e0b5303b4f4650e2c1033eaca6fe
rlence <ricardo.lence@digitalvirgo.com>
2021-02-16 14:06:08 +0100

    imagen redirect

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.scss';
 import '../../index.scss';
 import {connect} from 'react-redux';
-import { NavLink} from 'react-router-dom';
+import { NavLink, Redirect} from 'react-router-dom';
 
 function NavBar(props:any){
+
+    const [redirect, setRedirect] = useState({
+      state:false,
+      to:''
+    })
+
+    const handleClick = (e:any) => {
+      e.preventDefault();
+      console.log('haciendo click')
+      if(props.isAuth == true){
+        setRedirect({
+          state: true,
+          to:"/dashboard"
+        })
+      }else{
+        setRedirect({
+          state: true,
+          to:"/"
+        })
+      }
+
+    }
 
     return(
         <div className="cont-nav">
           <div className="row">
               <div className="img-nav">
-                <img src="https://www.pkfdcm.com/wp-content/uploads/2019/12/Cecotec_nuevo_programa_pagares_2019_.png" />
+                <img onClick={handleClick} src="https://www.pkfdcm.com/wp-content/uploads/2019/12/Cecotec_nuevo_programa_pagares_2019_.png" />
               </div>
              {props.path != '/' ?
                  <nav className="row nav-link">
@@ -20,13 +42,18 @@ function NavBar(props:any){
                : null
              }
           </div>
+          { redirect.state ?
+            <Redirect to={redirect.to} />
+            : null
+          }
         </div>
     )
 }
 
 const mapStateToProps = (state:any, props:any) => {
     return {
-        path:state.path
+        path:state.path,
+        isAuth:state.isAuth
     }
 }
 
